ceb1a4107a046c4ecdf4094083e1496c1cdbf134
info-vaik <info@vaikobo.com>
2023-02-22 17:54:59 +0900

    first commit

--- README.md
+++ README.md
@@ -38,3 +38,41 @@ python train_pb.py --train_font_dir_path ~/vaik-text-recognition-pb-trainer/data
 ![Screenshot from 2023-02-05 11-51-07](https://user-images.githubusercontent.com/116471878/216799082-20c838d7-e221-4172-b6cb-12676a3801e9.png)
 
 -----
+
+## export_fixed_model.py
+
+### Usage
+
+```shell
+python export_fixed_model.py --input_weight_path ~/.vaik_text_recognition_pb_trainer/output_model/2023-02-21-23-35-54/step-5000_batch-16_epoch-35_loss_0.3708_val_loss_0.1772/step-5000_batch-16_epoch-35_loss_0.3708_val_loss_0.1772 \
+                --char_json_path ~/vaik-text-recognition-pb-trainer/data/jpn_character.json \
+                --model_type simple_conv_model \
+                --image_height 96 \
+                --image_width 512 \
+                --output_dir_path ~/.vaik_text_recognition_pb_trainer/output_fixed_model
+```
+
+### Output
+
+![Screenshot from 2023-02-22 17-47-30](https://user-images.githubusercontent.com/116471878/220569300-4ad933da-5793-4e86-9253-cbfacafea3c4.png)
+
+-----
+
+## dump_dataset.py
+
+### Usage
+
+```shell
+python dump_dataset.py --train_font_dir_path ~/.vaik_text_recognition_pb_trainer/output_model/2023-02-21-23-35-54/step-5000_batch-16_epoch-35_loss_0.3708_val_loss_0.1772/step-5000_batch-16_epoch-35_loss_0.3708_val_loss_0.1772 \
+                --char_json_path ~/vaik-text-recognition-pb-trainer/data/jpn_character.json \
+                --model_type simple_conv_model \
+                --classes_json_path ~/vaik-text-recognition-pb-trainer/data/number_plate_address.json \
+                --image_height 96 \
+                --image_width 512 \
+                --sample_num 25000 \
+                --output_dir_path ~/.vaik_text_recognition_pb_trainer/dump_dataset
+```
+
+### Output
+
+![Screenshot from 2023-02-22 17-53-55](https://user-images.githubusercontent.com/116471878/220570348-36ea8bf5-013c-4fa3-bf02-c39f3b06a9fa.png)
---
+++ export_fixed_model.py
@@ -0,0 +1,55 @@
+import os
+import argparse
+import logging
+from datetime import datetime
+import pytz
+import tensorflow as tf
+
+from model import simple_conv_model, simple_tcn_model
+
+tf.get_logger().setLevel('ERROR')
+tf.debugging.disable_traceback_filtering()
+
+from data import text_image_dataset
+
+from callbacks import save_callback
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+
+model_dict = {
+    'simple_conv_model': simple_conv_model.prepare,
+    'simple_tcn_model': simple_tcn_model.prepare,
+}
+
+
+def export(input_weight_path, char_json_path, model_type, image_height, image_width, output_dir_path):
+    classes = text_image_dataset.TextImageDataset.char_json_read(char_json_path)
+
+    # prepare model
+    model, saved_model = model_dict[model_type](len(classes), (image_height, image_width, 3))
+    model.compile(optimizer=tf.keras.optimizers.SGD())
+    saved_model.load_weights(input_weight_path)
+
+    saved_model.save(output_dir_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='train pb')
+    parser.add_argument('--input_weight_path', type=str,
+                        default='~/.vaik_text_recognition_pb_trainer/output_model/2023-02-21-23-35-54/step-5000_batch-16_epoch-35_loss_0.3708_val_loss_0.1772/step-5000_batch-16_epoch-35_loss_0.3708_val_loss_0.1772')
+    parser.add_argument('--char_json_path', type=str,
+                        default=os.path.join(os.path.dirname(__file__), 'data/jpn_character.json'))
+    parser.add_argument('--model_type', type=str, default='simple_tcn_model')
+    parser.add_argument('--image_height', type=int, default=96)
+    parser.add_argument('--image_width', type=int, default=512)
+    parser.add_argument('--output_dir_path', type=str, default='~/.vaik_text_recognition_pb_trainer/output_fixed_model')
+    args = parser.parse_args()
+
+    args.input_weight_path = os.path.expanduser(args.input_weight_path)
+    args.char_json_path = os.path.expanduser(args.char_json_path)
+    args.output_dir_path = os.path.expanduser(args.output_dir_path)
+
+    os.makedirs(args.output_dir_path, exist_ok=True)
+
+    export(args.input_weight_path, args.char_json_path, args.model_type, args.image_height, args.image_width,
+           args.output_dir_path)
